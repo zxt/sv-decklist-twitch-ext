44fad8b685b8494575b70058b4ff84769ab30ecf
Zhao Tang <zxt@zhaotang.ca>
2021-06-01 21:28:02 -0400

    display decklist in panel view
    also reenable display of search icon + link in decklist even w/o tooltip

--- src/decklist.js
+++ src/decklist.js
@@ -58,36 +58,34 @@ function processDecklist(data, includeTooltips=true) {
         cardInfoDiv.appendChild(cardName);
         cardInfoDiv.appendChild(cardQty);
 
-        if(includeTooltips) {
-            const cardTooltip = document.createElement("p");
-            addClass(cardTooltip, "el-card-list-link");
+        const cardTooltip = document.createElement("p");
+        addClass(cardTooltip, "el-card-list-link");
 
-            const cardTooltipLink = document.createElement("a");
-            addClass(cardTooltipLink, "el-icon-search");
-            addClass(cardTooltipLink, "is-small");
-            cardTooltipLink.href = "https://shadowverse-portal.com/card/" + card.card_id;
-            cardTooltipLink.target = "_blank";
-            cardTooltipLink.dataset.cardTribeName = card.tribe_name;
-            cardTooltipLink.dataset.cardAtk = card.atk;
-            cardTooltipLink.dataset.cardEvoAtk = card.evo_atk;
-            cardTooltipLink.dataset.cardLife = card.life;
-            cardTooltipLink.dataset.cardEvoLife = card.evo_life;
-            cardTooltipLink.dataset.cardName = card.card_name;
-            cardTooltipLink.dataset.cardSkillDisc = card.skill_disc;
-            cardTooltipLink.dataset.cardEvoSkillDisc = card.evo_skill_disc;
-            cardTooltipLink.dataset.cardCharType = card.char_type;
-            cardTooltipLink.dataset.cardSrc = "https://shadowverse-portal.com/image/card/phase2/common/C/C_" + card.card_id + ".png";
-            cardTooltipLink.dataset.cardNameSrc = "https://shadowverse-portal.com/image/card/phase2/en/N/N_" + card.card_id + ".png";
-            
-            const cardSearchIcon = document.createElement("i");
-            addClass(cardSearchIcon, "icon-search");
-            addClass(cardSearchIcon, "is-small");
+        const cardTooltipLink = document.createElement("a");
+        addClass(cardTooltipLink, "el-icon-search");
+        addClass(cardTooltipLink, "is-small");
+        cardTooltipLink.href = "https://shadowverse-portal.com/card/" + card.card_id;
+        cardTooltipLink.target = "_blank";
+        cardTooltipLink.dataset.cardTribeName = card.tribe_name;
+        cardTooltipLink.dataset.cardAtk = card.atk;
+        cardTooltipLink.dataset.cardEvoAtk = card.evo_atk;
+        cardTooltipLink.dataset.cardLife = card.life;
+        cardTooltipLink.dataset.cardEvoLife = card.evo_life;
+        cardTooltipLink.dataset.cardName = card.card_name;
+        cardTooltipLink.dataset.cardSkillDisc = card.skill_disc;
+        cardTooltipLink.dataset.cardEvoSkillDisc = card.evo_skill_disc;
+        cardTooltipLink.dataset.cardCharType = card.char_type;
+        cardTooltipLink.dataset.cardSrc = "https://shadowverse-portal.com/image/card/phase2/common/C/C_" + card.card_id + ".png";
+        cardTooltipLink.dataset.cardNameSrc = "https://shadowverse-portal.com/image/card/phase2/en/N/N_" + card.card_id + ".png";
+        
+        const cardSearchIcon = document.createElement("i");
+        addClass(cardSearchIcon, "icon-search");
+        addClass(cardSearchIcon, "is-small");
 
-            cardTooltipLink.appendChild(cardSearchIcon);
-            cardTooltip.appendChild(cardTooltipLink);
+        cardTooltipLink.appendChild(cardSearchIcon);
+        cardTooltip.appendChild(cardTooltipLink);
 
-            cardInfoDiv.appendChild(cardTooltip);
-        };
+        cardInfoDiv.appendChild(cardTooltip);
 
         li.appendChild(bgImg);
         li.appendChild(cardInfoDiv);
--- src/panel.js
+++ src/panel.js
@@ -1,18 +1,33 @@
+import {getDecklist} from "./svportal.js";
+import {processDecklist} from "./decklist.js";
+
 import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", function(event) {
     const twitch = window.Twitch.ext;
+
+    twitch.onAuthorized( auth => {
+        //
+    });
 
     twitch.configuration.onChanged( () => {
         if(twitch.configuration.broadcaster) {
             try {
                 let hash = twitch.configuration.broadcaster.content;
 
+                getDecklist(hash)
+                .then(decklist => processDecklist(decklist, false));
+
             } catch (e) {
-                twitch.rig.log('Invalid config');
+                //
             }
         } else {
-            twitch.rig.log("no broadcaster config found");
+            //
         }
     })
+
+    twitch.listen("broadcast", ( (target, contentType, message) => {
+        getDecklist(message)
+        .then(decklist => processDecklist(decklist, false));
+    }));
 })
